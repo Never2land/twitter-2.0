04491293c9dff11c143583b6d2de7ea648e99299
never2land <ralfwang26@gmail.com>
2023-02-25 21:20:11 -0800

    Added tweet retrieve api

--- tweets/api/serializers.py
+++ tweets/api/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
-from tweets.models import Tweet
+
 from accounts.api.serializers import UserSerializerForTweet
+from comments.api.serializers import CommentSerializer
+from tweets.models import Tweet
 
 
 class TweetSerializer(serializers.ModelSerializer):
@@ -11,12 +13,20 @@ class TweetSerializer(serializers.ModelSerializer):
         fields = ('id', 'user', 'content', 'created_at')
 
 
+class TweetSerializerWithComments(TweetSerializer):
+    comments = CommentSerializer(source='comment_set', many=True)
+
+    class Meta:
+        model = Tweet
+        fields = ('id', 'user', 'content', 'comments', 'created_at')
+
+
 class TweetSerializerForCreate(serializers.ModelSerializer):
     content = serializers.CharField(min_length=6, max_length=144)
 
     class Meta:
         model = Tweet
-        # When there is only one field, it must be a tuple(need to add a comma 
+        # When there is only one field, it must be a tuple(need to add a comma
         # at the end).
         fields = ('content', )
 
